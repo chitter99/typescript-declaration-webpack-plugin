fb47ee604b3056be9ea55606568376084029371c
MrCodingB <bastiwoess@gmail.com>
2021-08-21 19:03:27 +0200

    Fix automatically exporting every declaration Fix log messages Add webpack types Fix multiline comments

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,11 +1,12 @@
+import { Compiler, optimize, sources } from 'webpack';
 
 type TypescriptDeclarationPluginOptions = {
-	out: string
-	removeMergedDeclarations: boolean
-	removeComments: boolean
-}
+	out: string;
+	removeMergedDeclarations: boolean;
+	removeComments: boolean;
+};
 
-export default class TypescriptDeclarationPlugin {
+export default class TypescriptDeclarationPlugin extends optimize.ModuleConcatenationPlugin {
 	public readonly name = 'TypeScriptDeclarationWebpackPlugin';
 
 	private regexImportDirective = /(\/{3} ?<reference types=")([A-Za-z0-9-]+)(" ?\/>)/;
@@ -17,139 +18,103 @@ export default class TypescriptDeclarationPlugin {
 		removeComments: true
 	};
 
-	constructor(options?) {
-		this.options = { ...this.options, ...options }
+	constructor(options?: TypescriptDeclarationPluginOptions) {
+		super(options);
+		this.options = { ...this.options, ...options };
 	}
 
-	log(m) {
-		console.log('[TypeScriptDeclarationPlugin] ' + m);
+	log(m: string) {
+		console.log(`[${this.name}] ${m}`);
 	}
 
-	mergeDeclarations(declarationFiles) {
-		let declarations = [], imports = [];
+	mergeDeclarations(declarationFiles: sources.Source[]) {
+		let mergedResult = '';
+		const moduleImports: { alias: string, lib: string; }[] = [];
+		const directiveImports: string[] = [];
 
-		for(let name in declarationFiles) {
-			let file = declarationFiles[name].source().split('\n'), modeMultilineComment = 0;
+		for (let name of declarationFiles) {
+			const file = name.source().toString('utf-8').split('\n');
+			let multilineCommentDepth = 0;
 
-			for(let i = 0; i < file.length; i++) {
-				let line = file[i], ignoreLine = false;
+			for (const line of file) {
+				if (multilineCommentDepth > 0) {
+					if (line.includes('*/')) {
+						multilineCommentDepth -= 1;
+					}
+					if (this.options.removeComments) {
+						continue;
+					}
+				} else {
+					if (line.startsWith('import')) {
+						if (line.includes(' as ')) {
+							// Module or UMD Import
+							const matches = this.regexImportModule.exec(line);
 
-				if(line.indexOf('declare') != -1 && line.indexOf('export') == -1) {
-					line = 'export ' + line;
-				} else if(line.indexOf('import') == 0) {
-					if(line.indexOf(' as ') != -1) {
-						// Module or UMD Import
-						let matches = this.regexImportModule.exec(line), importTracked = false;
-
-						// Make sure regex found matches
-						// Thanks to @dario-piotrowicz
-						if(matches) {
-							for(let im in imports) {
-								if(imports[im].mode == 'module' && imports[im].lib == matches[4] && imports[im].alias == matches[2]) {
-									importTracked = true;
-									break;
-								}
+							// Make sure regex found matches
+							// Thanks to @dario-piotrowicz
+							if (matches && !moduleImports.some((i) => i.alias === matches[2] && i.lib === matches[4])) {
+								moduleImports.push({ alias: matches[2], lib: matches[4] });
+								mergedResult += `import * as ${matches[2]} from "${matches[4]}";\n`;
 							}
-	
-							if(!importTracked) {
-								imports.push({
-									alias: matches[2],
-									lib: matches[4],
-									mode: 'module'
-								});
-							}	
 						}
+
+						continue;
 					}
-					ignoreLine = true;
-				} else if(line.indexOf('/// ') === 0) {
-					// Directive import
-					let matches = this.regexImportDirective.exec(line), importTracked = false;
-					
-					for(let im in imports) {
-						if(imports[im].mode == 'directive' && imports[im].lib == matches[2]) {
-							importTracked = true;
-							break;
+					if (line.startsWith('/// ')) {
+						// Directive import
+						const matches = this.regexImportDirective.exec(line);
+						if (matches && !directiveImports.includes(matches[2])) {
+							directiveImports.push(matches[2]);
+							mergedResult += `/// <reference types="${matches[2]}" />\n`;
 						}
+						continue;
 					}
-					if(!importTracked) {
-						imports.push({
-							lib: matches[2],
-							mode: 'directive'
-						});
+					if (line.startsWith('export') && line.includes('{}')) {
+						continue;
 					}
-					ignoreLine = true;
-				} else if(line.indexOf('export') == 0 && line.indexOf('{}') != -1) {
-					ignoreLine = true;
-				} else if(line.startsWith('//')) {
-					if(this.options.removeComments) {
-						ignoreLine = true;
-					}
-				} else if(line.indexOf('/*') != -1) {
-					if(this.options.removeComments) {
-						if(line.indexOf('*/') == -1) {
-							modeMultilineComment += 1;
-						}
-						ignoreLine = true;
-					}
-				} else if(line.indexOf('*/') != -1) {
-					if(this.options.removeComments) {
-						modeMultilineComment -= 1;
-						ignoreLine = true;
+					if (this.options.removeComments && line.startsWith('//')) {
+						continue;
 					}
 				}
 
-				if(!ignoreLine) {
-					declarations.push(line);
+				if (line.includes('/*')) {
+					multilineCommentDepth += 1;
+				}
+
+				if (!this.options.removeComments || multilineCommentDepth === 0) {
+					mergedResult += line + '\n';
 				}
 			}
 		}
 
-		let declarationImports = [];
-		imports.forEach(function(im) {
-			switch(im.mode) {
-				case 'module':
-					declarationImports.push('import * as ' + im.alias + ' from "' + im.lib + '";');
-					break;
-				case 'directive':
-					declarationImports.push('/// <reference types="' + im.lib + '" />');
-					break;
-			}
-		});
-
-		return declarationImports.concat(declarations).join('\n');
+		return mergedResult;
 	}
 
-	apply(compiler) {
+	apply(compiler: Compiler) {
 		compiler.hooks.emit.tapAsync(this.name, (compilation, callback) => {
 			// Search for declaration files.
-			var declarationFiles = {}, foundDeclaration = false;
-			for(var name in compilation.assets) {
+			const declarationFiles: sources.Source[] = [];
+			for (var name in compilation.assets) {
 				// Make sure declaration maps are ignored
-				if(name.endsWith('.d.ts')) {
-					declarationFiles[name] = compilation.assets[name];
-					if(this.options.removeMergedDeclarations) {
+				if (name.endsWith('.d.ts')) {
+					declarationFiles.push(compilation.assets[name]);
+					if (this.options.removeMergedDeclarations) {
 						delete compilation.assets[name];
 					}
-					foundDeclaration = true;
 				}
 			}
-			
-			if(!foundDeclaration) {
-				this.log('No TypeScript declarations has been found!');
-				this.log('Make sure declaration are activated in tsconfig.ts!');
-				return callback();
-			}
-			
-			let mergedDeclarationFile = this.mergeDeclarations(declarationFiles);
 
-			compilation.assets[this.options.out] = {
-				source: function() {
-					return mergedDeclarationFile;
-				},
-				size: function() {
-					return mergedDeclarationFile.length;
-				}
-			};
+			if (declarationFiles.length > 0) {
+				const mergedDeclarationFile = this.mergeDeclarations(declarationFiles);
+
+				compilation.assets[this.options.out] = {
+					source: () => mergedDeclarationFile,
+					size: () => mergedDeclarationFile.length
+				} as sources.Source;
+			} else {
+				this.log('No TypeScript declarations have been found.');
+				this.log('Make sure declarations are activated in tsconfig.json!');
+			}
 
 			callback();
 		});
